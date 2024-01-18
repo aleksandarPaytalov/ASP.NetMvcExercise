c8887a9bab733fa159c2d5586b9926d65bb81e02
aleksandarPaytalov <apaytalov89@gmail.com>
2024-01-18 21:23:59 +0200

    Add project files.

---
+++ BooksListEditor/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace BooksListEditor.Data;
+
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
